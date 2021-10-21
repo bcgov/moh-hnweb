1e33ba689cae31494f523b21116085cda2426a5c
AnumehaSrivastava05 <anumeha.saran@gmail.com>
2021-10-21 16:22:02 -0600

    TESTCAFE update
    1. Corrected id for manageEmployee link
    2. Added Role authentication
    3. Added functional testcases for checkEligibility
    4. Updated Employee page
    5. Updated Home page
    6. Added Alert page for message validation

---
+++ frontend/tests/e2e/tests/CheckEligibility.js
@@ -0,0 +1,81 @@
+import AlertPage from '../pages/AlertPage';
+import CheckEligibilityPage from '../pages/eligibility/CheckEligibilityPage';
+import {Role} from 'testcafe';
+
+const ERROR_MESSAGE = 'Please correct errors before submitting';
+const SITE_UNDER_TEST = 'http://localhost:3000/eligibility/checkEligibility'
+
+//TODO: import from roles/role.js
+const regularAccUser = Role(SITE_UNDER_TEST, async t => {
+    await t
+        .click('#zocial-moh_idp')
+        .typeText('#username', 'hnweb1')
+        .typeText('#password', process.env.TESTCAFE_PASSWORD)
+        .click("#kc-login");
+});
+
+fixture(`CheckEligibility Page`)
+.disablePageCaching `Test CheckEligibility`
+    .beforeEach(async t => {
+        await t
+            .useRole(regularAccUser)
+    })
+  .page('http://localhost:3000/eligibility/checkEligibility');
+
+test('Error when no username and date', async t => {
+    await t
+        .click(CheckEligibilityPage.submitButton)
+        .expect(AlertPage.alertBannerText.textContent).contains(ERROR_MESSAGE)
+
+        console.log("Error when no username and date");
+});
+
+test('Error when no date selected', async t => {
+    await t
+        .typeText(CheckEligibilityPage.phnInput, '123456')
+        .click(CheckEligibilityPage.submitButton)
+        .expect(AlertPage.alertBannerText.textContent).contains(ERROR_MESSAGE)
+
+        console.log("Error when no date selected");
+});
+
+test('Error when no username', async t => {
+    await t
+        .typeText(CheckEligibilityPage.eligibilityDate, '10/12/2021')
+        .click(CheckEligibilityPage.submitButton)
+        .expect(AlertPage.alertBannerText.textContent).contains(ERROR_MESSAGE)
+
+        console.log("Error when no uesrname");
+});
+
+test.skip('Check validation passed info', async t => {
+	await t
+        .typeText(CheckEligibilityPage.phnInput, '123456')
+        .click(CheckEligibilityPage.eligibilityDate)
+        .wait(1000)
+        .typeText(CheckEligibilityPage.eligibilityDate,'10/10/2021') 
+        .expect(CheckEligibilityPage.eligibilityDate.value).eql('10/10/2021')
+		.click(CheckEligibilityPage.submitButton)
+        .expect(AlertPage.alertBannerText.textContent).contains(ERROR_MESSAGE)
+
+		console.log("testcafe clicked submit button")
+});
+
+test('Check submitbutton is clickable', async t => {
+	await t
+        .typeText(CheckEligibilityPage.phnInput, '123456')  
+		.click(CheckEligibilityPage.submitButton)
+
+		console.log("testcafe clicked submit button")
+});
+
+test('Check cancelButton is clickable', async t => {
+	await t
+        .typeText(CheckEligibilityPage.phnInput, '123456')  
+		.click(CheckEligibilityPage.cancelButton)
+        .expect(CheckEligibilityPage.phnInput.value).eql('')
+        .expect(CheckEligibilityPage.eligibilityDate.value).eql('')	
+
+		console.log("testcafe clicked submit button")
+});
+
--- frontend/tests/e2e/tests/Employee.js
+++ frontend/tests/e2e/tests/Employee.js
@@ -1,11 +1,28 @@
 import Employee from '../pages/EmployeePage.js';
+import {Role} from 'testcafe';
 
+const SITE_UNDER_TEST = 'http://localhost:3000'
+
+//TODO: import { regularAccUser } from '../roles/roles';
+const regularAccUser = Role(SITE_UNDER_TEST, async t => {
+    await t
+        .click('#zocial-moh_idp')
+        .typeText('#username', 'hnweb1')
+        .typeText('#password', process.env.TESTCAFE_PASSWORD)
+        .click("#kc-login");
+});
 fixture(`Employee Page`)
+.disablePageCaching `Test Employee`
+    .beforeEach(async t => {
+        await t
+            .useRole(regularAccUser)
+    })
   .page('http://localhost:3000');
 
 test('Check Employee tab is clickable', async t => {
 	await t      
-		.click(Employee.employeeLink);			  
+		.click(Employee.employeeLink);
+
 		console.log("testcafe clicked Employee tab ")
 });
 
@@ -22,14 +39,14 @@ test('Check list of elements on Employee page', async t => {
 
 	await t
 		.click(Employee.employeeLink)
-		.expect(Employee.list.nth(2).innerText).eql('R30 - Add Employee')
-		.expect(Employee.list.nth(3).innerText).contains('R31 - Add Employee')
-		.expect(Employee.list.nth(4).innerText).eql('R32 - Get Contract Periods')
-		.expect(Employee.list.nth(5).innerText).eql('R34 - Update Employee Number and/or Department')
-		.expect(Employee.list.nth(6).innerText).eql('R35 - Cancel Employee')
-		.expect(Employee.list.nth(7).innerText).contains('R37- Get Employee')
-		.expect(Employee.list.nth(8).innerText).contains('R38 - Update Employee')
-		.expect(Employee.list.nth(9).innerText).eql('R40 - Contract Inquiry');
+		.expect(Employee.list.nth(6).innerText).eql('R30 - Add Employee')
+		.expect(Employee.list.nth(7).innerText).contains('R31 - Add Employee')
+		.expect(Employee.list.nth(8).innerText).eql('R32 - Get Contract Periods')
+		.expect(Employee.list.nth(9).innerText).eql('R34 - Update Employee Number and/or Department')
+		.expect(Employee.list.nth(10).innerText).eql('R35 - Cancel Employee')
+		.expect(Employee.list.nth(11).innerText).contains('R37- Get Employee')
+		.expect(Employee.list.nth(12).innerText).contains('R38 - Update Employee')
+		.expect(Employee.list.nth(13).innerText).eql('R40 - Contract Inquiry');
 
 		console.log('checked text for Employee list items');
  
