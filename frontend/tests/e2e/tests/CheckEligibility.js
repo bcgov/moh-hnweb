d83ab2c52899d8cc383b0cc6f46da16342d2b41f
Trevor Schiavone <trevor.schiavone@cgi.com>
2021-11-25 12:18:30 -0800

    test refactor
    removed duplicate tests,
    added comments
    standardized test names

--- frontend/tests/e2e/tests/CheckEligibility.js
+++ frontend/tests/e2e/tests/CheckEligibility.js
@@ -5,7 +5,8 @@ import { SITE_UNDER_TEST } from '../configuration';
 
 const ERROR_MESSAGE = 'Please correct errors before submitting';
 const SUCCESS_MESSAGE = 'Search complete';
-const PHN_ERROR_MESSAGE = 'PHN is required'
+const PHN_REQUIRED_MESSAGE = 'PHN is required'
+const DATE_TO_CHECK_REQUIRED_MESSAGE = 'Date to Check is required'
 const INVALID_PHN_ERROR_MESSAGE = 'PHN format is invalid';
 
 const PAGE_TO_TEST = SITE_UNDER_TEST + '/eligibility/checkEligibility'
@@ -18,58 +19,50 @@ fixture(`CheckEligibility Page`)
     })
     .page(PAGE_TO_TEST);
 
-test('Error when no phn and date', async t => {
+test('Check required fields validation', async t => {
     await t
-        .click(CheckEligibilityPage.submitButton)
-        .expect(AlertPage.alertBannerText.textContent).contains(ERROR_MESSAGE)
-});
-
-test('Error when no date selected', async t => {
-    await t 
-        .typeText(CheckEligibilityPage.phnInput, '9000448000')
-        .click(CheckEligibilityPage.eligibilityDate)
+        // Given required fields aren't filled out (phn, date to check)
+        .selectText(CheckEligibilityPage.eligibilityDate)
+        .pressKey('delete')
         .pressKey('tab')
+        // When I click the submit button
         .click(CheckEligibilityPage.submitButton)
+        // I expect an error message stating the page had errors and individual error messages for each required field
         .expect(AlertPage.alertBannerText.textContent).contains(ERROR_MESSAGE)
+        .expect(CheckEligibilityPage.ErrorText.nth(0).textContent).contains(PHN_REQUIRED_MESSAGE)
+        .expect(CheckEligibilityPage.ErrorText.nth(1).textContent).contains(DATE_TO_CHECK_REQUIRED_MESSAGE)
 });
 
-test('Error when no phn', async t => {
+test('Check invalid phn format validation', async t => {
     await t
-        .click(CheckEligibilityPage.eligibilityDate)
-        .pressKey('tab')       
-        .click(CheckEligibilityPage.submitButton)
-        .expect(CheckEligibilityPage.ErrorText.textContent).contains(PHN_ERROR_MESSAGE)
-        .expect(AlertPage.alertBannerText.textContent).contains(ERROR_MESSAGE)
-});
-
-test('Check invalid phn format error message', async t => {
-    await t
+        // Given a PHN entered with an invalid format
         .typeText(CheckEligibilityPage.phnInput, '9000448000')
+        // When I click the submit button
 		.click(CheckEligibilityPage.submitButton)
+        // I expect an error message stating the page had errors and an individual error message for the PHN format
         .expect(CheckEligibilityPage.ErrorText.nth(0).textContent).contains(INVALID_PHN_ERROR_MESSAGE)
         .expect(AlertPage.alertBannerText.textContent).contains(ERROR_MESSAGE)
 });
 
-test('Check validation passed info', async t => {
+test('Check properly filled form passes validation', async t => {
     await t
+        // Given the page is filled out correctly
         .typeText(CheckEligibilityPage.phnInput, '9306448169')
         .click(CheckEligibilityPage.eligibilityDate)
         .pressKey('tab')
-        .expect(CheckEligibilityPage.eligibilityDate.value).notEql('')
+        // When I click the submit button
 		.click(CheckEligibilityPage.submitButton)
+        // I expect a success message
         .expect(AlertPage.alertBannerText.textContent).contains(SUCCESS_MESSAGE)
 });
 
-test('Check submit button is clickable', async t => {
+test('Check cancel button clears the form', async t => {
 	await t
+        // Given I have a form filled out with data
         .typeText(CheckEligibilityPage.phnInput, '9000448000')
-		.click(CheckEligibilityPage.submitButton)
-});
-
-test('Check cancel button is clickable', async t => {
-	await t
-        .typeText(CheckEligibilityPage.phnInput, '9000448000')
+        // When I click the cancel button
 		.click(CheckEligibilityPage.cancelButton)
+        // I expect the form to be cleared
         .expect(CheckEligibilityPage.phnInput.value).eql('')
 });
 
--- frontend/tests/e2e/tests/CoverageStatusCheck.js
+++ frontend/tests/e2e/tests/CoverageStatusCheck.js
@@ -6,10 +6,10 @@ import { regularAccUser } from '../roles/roles';
 import { SITE_UNDER_TEST } from '../configuration';
 
 const ERROR_MESSAGE = 'Please correct errors before submitting';
-const PHN_ERROR_MESSAGE = 'PHN is required';
-const DOB_ERROR_MESSAGE = 'Date Of Birth is required';
+const PHN_REQUIRED_MESSAGE = 'PHN is required';
+const DOB_REQUIRED_MESSAGE = 'Date Of Birth is required';
 const INVALID_DOB_ERROR_MESSAGE = 'Date of Birth must not be in the future';
-const DOS_ERROR_MESSAGE = 'Date Of Service is required';
+const DOS_REQUIRED_MESSAGE = 'Date Of Service is required';
 const INVALID_PHN_ERROR_MESSAGE = 'PHN format is invalid';
 const SUCCESS_MESSAGE = 'Search complete';
 const TRANS_SUCCESSFUL = 'Transaction Successful';
@@ -30,149 +30,109 @@ fixture(`Coverage Status Check Page`)
     })
    .page(PAGE_TO_TEST);
 
-test('Error when no phn, dateOfBirth and dateOfService', async t => {
+test('Check required fields validation', async t => {
     await t
+        // Given required fields aren't filled out (phn, date of birth, and date of service)
         .selectText(CoverageStatusCheckPage.dateOfServiceInput)
         .pressKey('delete')
         .pressKey('tab')
+        // When I click the submit button
         .click(CoverageStatusCheckPage.submitButton)
-        .expect(CoverageStatusCheckPage.ErrorText.nth(0).textContent).contains(PHN_ERROR_MESSAGE)
-        .expect(CoverageStatusCheckPage.ErrorText.nth(1).textContent).contains(DOB_ERROR_MESSAGE)
-        .expect(CoverageStatusCheckPage.ErrorText.nth(2).textContent).contains(DOS_ERROR_MESSAGE)
+        // I expect an error message stating the page had errors and individual error messages for each required field
+        .expect(CoverageStatusCheckPage.ErrorText.nth(0).textContent).contains(PHN_REQUIRED_MESSAGE)
+        .expect(CoverageStatusCheckPage.ErrorText.nth(1).textContent).contains(DOB_REQUIRED_MESSAGE)
+        .expect(CoverageStatusCheckPage.ErrorText.nth(2).textContent).contains(DOS_REQUIRED_MESSAGE)
         .expect(AlertPage.alertBannerText.textContent).contains(ERROR_MESSAGE)
 });
 
-test('Error when no phn selected', async t => {
+test('Check invalid phn format validation', async t => {
     await t
-        .typeText(CoverageStatusCheckPage.dateOfBirthInput, '20211108')
-        .click(CoverageStatusCheckPage.dateOfServiceInput)
-        .pressKey('tab')    
-        .click(CoverageStatusCheckPage.submitButton)
-        .expect(AlertPage.alertBannerText.textContent).contains(ERROR_MESSAGE)
-});
-
-test('Error when no dateOfBirth selected', async t => {
-    await t
-        .typeText(CoverageStatusCheckPage.phnInput, '9306448169')
-        .click(CoverageStatusCheckPage.dateOfServiceInput)
-        .pressKey('tab') 
-        .click(CoverageStatusCheckPage.submitButton)
-        .expect(AlertPage.alertBannerText.textContent).contains(ERROR_MESSAGE)
-});
-
-test('Error when no dateOfService selected', async t => {
-    await t
-        .typeText(CoverageStatusCheckPage.phnInput, '9306448169')
-        .typeText(CoverageStatusCheckPage.dateOfBirthInput, '20211108') 
-        .selectText(CoverageStatusCheckPage.dateOfServiceInput)
-        .pressKey('delete')
-        .pressKey('tab')
-        .click(CoverageStatusCheckPage.submitButton)
-        .expect(AlertPage.alertBannerText.textContent).contains(ERROR_MESSAGE)
-});
-
-test('Error when no phn and dateOfService selected', async t => {
-    await t
-        .typeText(CoverageStatusCheckPage.dateOfBirthInput, '20211108')
-        .selectText(CoverageStatusCheckPage.dateOfServiceInput)
-        .pressKey('delete')
-        .pressKey('tab') 
-        .click(CoverageStatusCheckPage.submitButton)
-        .expect(AlertPage.alertBannerText.textContent).contains(ERROR_MESSAGE)
-});
-
-test('Error when no phn and dateofBirth selected', async t => {
-    await t
-        .click(CoverageStatusCheckPage.dateOfServiceInput)
-        .pressKey('tab')   
-        .click(CoverageStatusCheckPage.submitButton)
-        .expect(AlertPage.alertBannerText.textContent).contains(ERROR_MESSAGE)
-});
-
-test('Check invalid phn format error message', async t => {
-
-    await t
-        .typeText(CoverageStatusCheckPage.phnInput, '9876543211')
-        .typeText(CoverageStatusCheckPage.dateOfBirthInput, '20211108') 
-        .click(CoverageStatusCheckPage.dateOfServiceInput)
-        .pressKey('tab')   
-        .expect(CoverageStatusCheckPage.dateOfServiceInput.value).notEql('')
+        // Given a PHN entered with an invalid format
+        .typeText(CoverageStatusCheckPage.phnInput, '9000444000')
+        // When I click the submit button
 		.click(CoverageStatusCheckPage.submitButton)
+        // I expect an error message stating the page had errors and an individual error message for the PHN format
         .expect(CoverageStatusCheckPage.ErrorText.nth(0).textContent).contains(INVALID_PHN_ERROR_MESSAGE)
         .expect(AlertPage.alertBannerText.textContent).contains(ERROR_MESSAGE)
 });
 
-test('Check future date of birth error message', async t => {
+test('Check date of birth in future validation', async t => {
     await t
+        // Given a date of birth entered in the future
         .typeText(CoverageStatusCheckPage.phnInput, '9306448169')
-        .typeText(CoverageStatusCheckPage.dateOfBirthInput, '20291108') 
-        .click(CoverageStatusCheckPage.dateOfServiceInput)
-        .pressKey('tab')   
-        .expect(CoverageStatusCheckPage.dateOfServiceInput.value).notEql('')
+        .typeText(CoverageStatusCheckPage.dateOfBirthInput, '20391108')
+        // When I click the submit button
 		.click(CoverageStatusCheckPage.submitButton)
+        // I expect an error message stating the page had errors and an individual error message for the DoB format
         .expect(CoverageStatusCheckPage.ErrorText.nth(0).textContent).contains(INVALID_DOB_ERROR_MESSAGE)
         .expect(AlertPage.alertBannerText.textContent).contains(ERROR_MESSAGE)
 });
 
 
-test('Check validation passed info', async t => {
+test('Check properly filled form passes validation', async t => {
     await t
+        // Given the page is filled out correctly
         .typeText(CoverageStatusCheckPage.phnInput, '9306448169')
         .typeText(CoverageStatusCheckPage.dateOfBirthInput, '20211108') 
         .click(CoverageStatusCheckPage.dateOfServiceInput)
-        .pressKey('tab')   
+        .pressKey('tab')
+        // When I click the submit button
         .expect(CoverageStatusCheckPage.dateOfServiceInput.value).notEql('')
 		.click(CoverageStatusCheckPage.submitButton)
+        // I expect a success message
         .expect(AlertPage.alertBannerText.textContent).contains(SUCCESS_MESSAGE)
         .expect(Selector('h2').textContent).contains(TRANS_SUCCESSFUL)
 });
 
-test('Check Subsidy Insured Service CheckBox is checked/unchecked ', async t => { 
+test('Check Subsidy Insured Service CheckBox check and uncheck work', async t => {
     await makeVisible('checkSubsidyInsuredService');  
   	await t
+        // When I click a checkbox
+        // I expect the status to toggle
 		.click(CoverageStatusCheckPage.subsidyInsuredServiceCheckBox)  //Check box
         .expect(CoverageStatusCheckPage.subsidyInsuredServiceCheckBox.checked).ok()  //Confirm whether the option is selected
         .click(CoverageStatusCheckPage.subsidyInsuredServiceCheckBox)
         .expect(CoverageStatusCheckPage.subsidyInsuredServiceCheckBox.checked).notOk();
 });
 
-test('Check Date Of Last Eye Examination CheckBox is checked/unchecked', async t => { 
+test('Check Date Of Last Eye Examination CheckBox check and uncheck work', async t => {
     await makeVisible('checkLastEyeExam');  
 	await t
+        // When I click a checkbox
+        // I expect the status to toggle
 		.click(CoverageStatusCheckPage.dateOfLastEyeExaminationCheckBox)  //Check box
         .expect(CoverageStatusCheckPage.dateOfLastEyeExaminationCheckBox.checked).ok()  //Confirm whether the option is selected
         .click(CoverageStatusCheckPage.dateOfLastEyeExaminationCheckBox)
         .expect(CoverageStatusCheckPage.dateOfLastEyeExaminationCheckBox.checked).notOk();
 });
 
-test('Check Patient Restriction CheckBox is checked/unchecked', async t => { 
+test('Check Patient Restriction CheckBox check and uncheck work', async t => {
     await makeVisible('checkPatientRestriction');   
 	await t
+        // When I click a checkbox
+        // I expect the status to toggle
 		.click(CoverageStatusCheckPage.patientRestrictionCheckBox)  //Check box
         .expect(CoverageStatusCheckPage.patientRestrictionCheckBox.checked).ok()  //Confirm whether the option is selected
         .click(CoverageStatusCheckPage.patientRestrictionCheckBox)
         .expect(CoverageStatusCheckPage.patientRestrictionCheckBox.checked).notOk();
 });
 
-test('Check submit button is clickable', async t => {
-	await t
-        .typeText(CoverageStatusCheckPage.phnInput, '9306448169')  
-		.click(CoverageStatusCheckPage.submitButton)
-});
-
-test('Check cancelButton is clickable', async t => {
+test('Check cancel button clears the form', async t => {
     await makeVisible('checkSubsidyInsuredService');
     await makeVisible('checkLastEyeExam');
     await makeVisible('checkPatientRestriction');
 	await t
-        .typeText(CoverageStatusCheckPage.phnInput, '9306448169') 
+        // Given I have a form filled out with data
+        .typeText(CoverageStatusCheckPage.phnInput, '9000444000')
         .typeText(CoverageStatusCheckPage.dateOfBirthInput, '20211108') 
         .click(CoverageStatusCheckPage.dateOfServiceInput)
         .pressKey('tab')
         .click(CoverageStatusCheckPage.subsidyInsuredServiceCheckBox) 
         .click(CoverageStatusCheckPage.dateOfLastEyeExaminationCheckBox) 
-        .click(CoverageStatusCheckPage.patientRestrictionCheckBox)  
+        .click(CoverageStatusCheckPage.patientRestrictionCheckBox)
+        // When I click the cancel button
 		.click(CoverageStatusCheckPage.cancelButton)
+        // I expect the form to be cleared
         .expect(CoverageStatusCheckPage.phnInput.value).eql('')
         .expect(CoverageStatusCheckPage.dateOfBirthInput.value).eql('')	
         .expect(CoverageStatusCheckPage.subsidyInsuredServiceCheckBox.checked).notOk()
