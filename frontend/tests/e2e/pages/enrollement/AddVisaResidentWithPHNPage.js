a95149f0faf646eddb342c7e3cafad7aa5d15e75
AnumehaSrivastava05 <anumeha.saran@gmail.com>
2021-12-07 12:08:36 -0700

    Added testcafe
    1. Added page and test scripts for Person details and AddVisaResidentWithPHN

---
+++ frontend/tests/e2e/pages/enrollement/AddVisaResidentWithPHNPage.js
@@ -0,0 +1,40 @@
+import { Selector } from 'testcafe';
+
+class AddVisaResidentWithPHN {
+    constructor () {
+        
+        this.groupNumberInput = Selector('#groupNumber');
+        this.immigrationCodeSelect  = Selector('#immigrationCode');
+        this.groupMemberNumberInput = Selector('#groupMemberNumber');
+        this.visaIssueDateInput = Selector('div#visaIssueDate > div > div > div > input');
+        this.departmentNumberInput = Selector('#departmentNumber');
+        this.visaExpiryDateInput = Selector('div#visaExpiryDate > div > div > div > input');
+        this.residenceDateInput = Selector('div#residenceDate > div > div > div > input'); 
+        this.coverageEffectiveDateInput = Selector('div#coverageEffectiveDate > div > div > div > input');
+        this.telephoneInput = Selector('#telephone');
+        this.coverageCancellationDateInput = Selector('div#coverageCancellationDate > div > div > div > input');
+        this.address1Input = Selector('#address1');
+        this.address2Input = Selector('#address2');
+        this.address3Input = Selector('#address3');
+        this.cityInput = Selector('#city');
+        this.provinceSelect = Selector('#province');
+        //this.provinceOptionInput = provinceInput.find('option');
+        this.postalCodeInput = Selector('#postalCode');
+        this.mailingAddress1Input = Selector('#mailingAddress1');
+        this.mailingAddress2Input = Selector('#mailingAddress2');
+        this.mailingAddress3Input = Selector('#mailingAddress3');
+        this.mailingCityInput = Selector('#mailingCity');
+        this.mailingProvinceInput = Selector('#mailingProvince');
+        this.mailingPostalCodeInput = Selector('#mailingPostalCode');
+        this.priorResidenceCodeInput = Selector('#priorResidenceCode');
+        this.otherProvinceHealthcareNumberInput = Selector('#otherProvinceHealthcareNumber');
+
+        this.ErrorText = Selector('div').withAttribute('class','error-text');
+
+        this.submitButton = Selector('button[type="submit"]');
+        this.cancelButton = Selector('button[type="button"]');
+        
+    }
+}
+
+export default new AddVisaResidentWithPHN();
