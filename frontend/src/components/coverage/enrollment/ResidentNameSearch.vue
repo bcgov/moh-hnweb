c492c6305f8a2cd68ba01cd4e2a3a413aeb5cda8
daveb-hni <80367247+daveb-hni@users.noreply.github.com>
2021-12-14 13:21:49 -0800

    4882 Add visa resident without PHN initial check-in

---
+++ frontend/src/components/coverage/enrollment/ResidentNameSearch.vue
@@ -0,0 +1,135 @@
+<template>
+  <div>
+    <form @submit.prevent="submitForm">
+      <AppRow>
+        <AppCol class="col3">
+          <AppInput :e-model="v$.surname" id="surname" label="Surname" type="text" v-model.trim="surname" />
+        </AppCol>
+      </AppRow>
+      <AppRow>
+        <AppCol class="col3">
+          <AppInput :e-model="v$.firstName" id="firstName" label="First Name" type="text" v-model.trim="firstName" />
+        </AppCol>
+      </AppRow>
+      <AppRow>
+        <AppCol class="col3">
+          <AppInput :e-model="v$.secondName" id="secondName" label="Second Name (Optional)" type="text" v-model.trim="secondName" />
+        </AppCol>
+      </AppRow>
+      <AppRow>
+        <AppCol class="col4">
+          <AppDateInput :e-model="v$.dateOfBirth" id="dateOfBirth" label="Date of Birth" v-model="dateOfBirth" />
+        </AppCol>
+      </AppRow>
+      <AppRow>
+        <AppCol class="col4">
+          Main Screen Gender: {{ gender }}
+          <AppRadioButtonGroup :e-model="v$.gender" id="gender" label="Gender" :group="this.GENDER_RADIO_BUTTON_GROUP" v-model="gender" />
+        </AppCol>
+      </AppRow>
+      <AppRow>
+        <AppButton :disabled="searching" mode="primary" type="submit">Submit</AppButton>
+        <AppButton @click="resetForm" mode="secondary" type="button">Clear</AppButton>
+      </AppRow>
+    </form>
+  </div>
+</template>
+
+<script>
+import AppButton from '../../AppButton.vue'
+import AppCol from '../../grid/AppCol.vue'
+import AppRow from '../../grid/AppRow.vue'
+import AppDateInput from '../../AppDateInput.vue'
+import AppInput from '../../AppInput.vue'
+import AppRadioButtonGroup from '../../ui/AppRadioButtonGroup.vue'
+import useVuelidate from '@vuelidate/core'
+import { validatePHN, VALIDATE_PHN_MESSAGE } from '../../../util/validators'
+import { required, helpers } from '@vuelidate/validators'
+
+export default {
+  name: 'ResidentNameSearch',
+  components: {
+    AppButton,
+    AppCol,
+    AppRow,
+    AppInput,
+    AppDateInput,
+    AppRadioButtonGroup,
+  },
+  setup() {
+    return {
+      v$: useVuelidate(),
+    }
+  },
+  data() {
+    return {
+      surname: '',
+      firstName: '',
+      secondName: '',
+      dateOfBirth: null,
+      gender: null,
+      searching: false,
+    }
+  },
+  created() {
+    this.GENDER_RADIO_BUTTON_GROUP = [
+      { label: 'Male', value: 'male' },
+      { label: 'Female', value: 'female' },
+      { label: 'Unknown', value: 'unknown' },
+    ]
+  },
+  computed: {
+    inputClass() {
+      return {
+        'error-input': this.v$.gender.$error,
+      }
+    },
+  },
+  methods: {
+    async submitForm() {
+      this.searching = true
+      try {
+        const isValid = await this.v$.$validate()
+        if (!isValid) {
+          this.$store.commit('alert/setErrorAlert')
+          this.searching = false
+          return
+        }
+        this.$emit('update-resident', { surname, firstName, secondName, dateOfBirth, gender })
+      } catch (err) {
+        console.log(`Error ${err}`)
+        this.$store.commit('alert/setErrorAlert', `${err}`)
+      } finally {
+        this.searching = false
+      }
+    },
+    resetForm() {
+      this.surname = ''
+      this.firstName = ''
+      this.secondName = ''
+      this.dateOfBirth = null
+      this.gender = null
+      this.v$.$reset()
+      this.$store.commit('alert/dismissAlert')
+      this.searching = false
+    },
+  },
+  validations() {
+    return {
+      surname: {
+        required,
+      },
+      firstName: {
+        required,
+      },
+      secondName: {},
+      dateOfBirth: {
+        required,
+      },
+      gender: {
+        required,
+      },
+    }
+  },
+}
+</script>
---
+++ frontend/src/components/ui/AppRadioButtonGroup.vue
@@ -0,0 +1,55 @@
+<template>
+  <div>
+    <div class="text_label">
+      <label>{{ label }}</label>
+    </div>
+    <div :class="inputClass">
+      <AppRadioButton v-for="item in this.group" :id="item.value" :label="item.label" :value="item.value" v-model="modelValue" />
+    </div>
+    <div class="error-text" v-for="error in eModel.$errors">{{ error.$message.replace('Value', label) }}</div>
+  </div>
+</template>
+<script>
+import AppRadioButton from '../AppRadioButton.vue'
+export default {
+  name: 'AppRadioButtonGroup',
+  components: {
+    AppRadioButton,
+  },
+  computed: {
+    inputClass() {
+      return {
+        'error-input': this.eModel.$error,
+      }
+    },
+  },
+  props: {
+    eModel: Object,
+    label: String,
+    modelValue: String,
+    group: [],
+  },
+  watch: {
+    modelValue: function (val) {
+      //update the model when the radio selection changes.
+      if (val) {
+        this.$emit('update:modelValue', val)
+      }
+    },
+  },
+}
+</script>
+
+<style scoped>
+.text_label {
+  display: flex;
+}
+.error-input {
+  border: 2px solid #606060;
+  border-color: #d8292f !important;
+}
+
+.error-text {
+  color: #d8292f;
+}
+</style>
