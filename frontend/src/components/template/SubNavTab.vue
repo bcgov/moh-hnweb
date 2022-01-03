6864a5cf8c2038302a65f8f8acadb143f4846aee
daveb-hni <80367247+daveb-hni@users.noreply.github.com>
2022-01-03 13:30:49 -0800

    Added clearing of all store items on tab nav

--- frontend/src/components/template/SubNavTab.vue
+++ frontend/src/components/template/SubNavTab.vue
@@ -1,28 +1,28 @@
 <template>
-  <router-link @click="resetAlert" :to="{ name: this.routeName}">{{title}}</router-link>
+  <router-link @click="resetStore" :to="{ name: this.routeName }">{{ title }}</router-link>
 </template>
 
 <script>
-  export default {
-    name: "SubNavTab",
-    props: {
-      routeName: String,
-      title: String
+export default {
+  name: 'SubNavTab',
+  props: {
+    routeName: String,
+    title: String,
+  },
+  methods: {
+    resetStore() {
+      this.$store.commit('alert/dismissAlert')
+      this.$store.commit('studyPermitHolder/resetResident')
     },
-    methods: {
-      resetAlert() {
-       this.$store.commit('alert/dismissAlert');
-      }
-    }
-  }
+  },
+}
 </script>
 
 <style scoped>
-
 a {
-  background: #999999; 
+  background: #999999;
   border-radius: 8px 8px 0 0;
-  color: #ffffff; 
+  color: #ffffff;
   display: inline-block;
   padding: 0 20px;
   height: 36px;
@@ -33,11 +33,10 @@ a {
   text-decoration: none;
 }
 a:hover {
-    background: #666666;
+  background: #666666;
 }
 .router-link-exact-active,
 .router-link-exact-active:hover {
-    background: #003366;
+  background: #003366;
 }
-
 </style>
