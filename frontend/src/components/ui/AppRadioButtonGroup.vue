7b486a701155df6763e692af37121e1d5040a4c3
daveb-hni <80367247+daveb-hni@users.noreply.github.com>
2022-01-03 17:25:13 -0800

    Moved Radio Button component to ui directory

--- frontend/src/components/ui/AppRadioButtonGroup.vue
+++ frontend/src/components/ui/AppRadioButtonGroup.vue
@@ -10,7 +10,7 @@
   </div>
 </template>
 <script>
-import AppRadioButton from '../AppRadioButton.vue'
+import AppRadioButton from './AppRadioButton.vue'
 export default {
   name: 'AppRadioButtonGroup',
   components: {
