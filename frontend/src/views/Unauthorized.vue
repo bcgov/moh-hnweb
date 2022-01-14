d5942b7a24449d1e0bd682435de0a021d606d397
daveb-hni <80367247+daveb-hni@users.noreply.github.com>
2022-01-14 15:13:24 -0800

    Removed actual contact info for now as it is not applicable to this app's users

--- frontend/src/views/Unauthorized.vue
+++ frontend/src/views/Unauthorized.vue
@@ -4,8 +4,8 @@
     <p>We're sorry, but you do not have access to the requested resource</p>
     <p>
       Please contact the Help Desk if you require further assistance<br />
-      Phone number: 250-387-7000<br />
-      Email: 77000@gov.bc.ca
+      Phone number: xxx-xxx-xxxx<br />
+      Email: --@gov.bc.ca
     </p>
   </div>
 </template>
