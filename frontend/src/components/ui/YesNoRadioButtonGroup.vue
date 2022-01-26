15810de3005eff68364a196c5b7de26bdc60730f
daveb-hni <80367247+daveb-hni@users.noreply.github.com>
2022-01-25 20:43:09 -0800

    Create YesNoRadioButtonGroup.vue

---
+++ frontend/src/components/ui/YesNoRadioButtonGroup.vue
@@ -0,0 +1,36 @@
+<template>
+  <AppRadioButtonGroup :e-model="eModel" :id="id" :label="label" :group="this.YES_NO_RADIO_BUTTON_GROUP" :tooltip="tooltip" :tooltipText="tooltipText" v-model="modelValue" />
+</template>
+
+<script>
+import AppRadioButtonGroup from './AppRadioButtonGroup.vue'
+
+export default {
+  name: 'YesNoRadioButtonGroup',
+  components: {
+    AppRadioButtonGroup,
+  },
+  props: {
+    eModel: Object,
+    label: { default: 'Choose Yes or No', type: String },
+    id: { default: 'yesNo', type: String },
+    modelValue: String,
+    tooltip: Boolean,
+    tooltipText: String,
+  },
+  created() {
+    this.YES_NO_RADIO_BUTTON_GROUP = [
+      { label: 'Yes', value: 'Y' },
+      { label: 'No', value: 'N' },
+    ]
+  },
+  watch: {
+    modelValue: function (val) {
+      //update the model
+      if (val) {
+        this.$emit('update:modelValue', val)
+      }
+    },
+  },
+}
+</script>
