3db0f9e0178c8d1548eb30404ca1854b2a8bf904
daveb-hni <80367247+daveb-hni@users.noreply.github.com>
2022-02-04 16:24:18 -0800

    Adding version in footer using env config

--- frontend/src/components/template/TheFooter.vue
+++ frontend/src/components/template/TheFooter.vue
@@ -1,34 +1,44 @@
 <template>
   <footer class="footer">
     <div class="container">
-    <ul>
-      <li>
-        <router-link :to="{ name: 'Home'}">Home</router-link>
-      </li>
-      <li>
-        <a href="http://www2.gov.bc.ca/gov/content/home/disclaimer" target="_blank">Disclaimer</a>
-      </li>
-      <li>
-        <a href="http://www2.gov.bc.ca/gov/content/home/privacy" target="_blank">Privacy</a>
-      </li>
-      <li>
-        <a href="http://www2.gov.bc.ca/gov/content/home/accessibility" target="_blank">Accessibility</a>
-      </li>
-      <li>
-        <a href="http://www2.gov.bc.ca/gov/content/home/copyright" target="_blank">Copyright</a>
-      </li>
-      <li>
-        <a href="https://www2.gov.bc.ca/gov/content/home/get-help-with-government-services" target="_blank">Contact Us</a>
-      </li>
-    </ul>
+      <div>
+        <ul>
+          <li>
+            <router-link :to="{ name: 'Home' }">Home</router-link>
+          </li>
+          <li>
+            <a href="http://www2.gov.bc.ca/gov/content/home/disclaimer" target="_blank">Disclaimer</a>
+          </li>
+          <li>
+            <a href="http://www2.gov.bc.ca/gov/content/home/privacy" target="_blank">Privacy</a>
+          </li>
+          <li>
+            <a href="http://www2.gov.bc.ca/gov/content/home/accessibility" target="_blank">Accessibility</a>
+          </li>
+          <li>
+            <a href="http://www2.gov.bc.ca/gov/content/home/copyright" target="_blank">Copyright</a>
+          </li>
+          <li>
+            <a href="https://www2.gov.bc.ca/gov/content/home/get-help-with-government-services" target="_blank">Contact Us</a>
+          </li>
+        </ul>
+      </div>
+      <div class="version">
+        <span>{{ this.version }}</span>
+      </div>
     </div>
   </footer>
 </template>
 
 <script>
-  export default {
-    name: "TheFooter"
-  }
+export default {
+  name: 'TheFooter',
+  data() {
+    return {
+      version: import.meta.env.VITE_APP_VERSION,
+    }
+  },
+}
 </script>
 
 <style scoped>
@@ -36,15 +46,21 @@ footer {
   background-color: #036;
   border-top: 2px solid #fcba19;
   color: #fff;
-  font-family: ‘BCSans’, ‘Noto Sans’, Verdana, Arial, sans-serif; 
+  font-family: ‘BCSans’, ‘Noto Sans’, Verdana, Arial, sans-serif;
 }
 
 footer .container {
+  display: flex;
+  justify-content: space-between;
   height: 46px;
   padding: 10px 60px;
   max-width: 1320px;
   min-width: 1100px;
   margin: 0 auto;
+}
+
+footer .version {
+  font-size: 0.813em;
 }
 
 footer ul {
@@ -60,7 +76,7 @@ footer ul {
 
 footer ul li a {
   font-size: 0.813em;
-  font-weight: normal;  /* 400 */
+  font-weight: normal; /* 400 */
   color: #fff;
   border-right: 1px solid #4b5e7e;
   padding-left: 5px;
@@ -74,7 +90,7 @@ a:hover {
 }
 
 :focus {
-  outline: 4px solid #3B99FC;
+  outline: 4px solid #3b99fc;
   outline-offset: 1px;
 }
 </style>
