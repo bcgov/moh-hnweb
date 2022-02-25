9f2afadb676bcfc6abb35829173d5404af8a37dc
AnumehaSrivastava05 <anumeha.saran@gmail.com>
2022-02-25 11:16:33 -0700

    R40: Removed unused imports

--- backend/src/main/java/ca/bc/gov/hlth/hnweb/controller/MspContractsController.java
+++ backend/src/main/java/ca/bc/gov/hlth/hnweb/controller/MspContractsController.java
@@ -9,18 +9,14 @@ import javax.validation.Valid;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.reactive.function.client.WebClientException;
-import org.springframework.web.server.ResponseStatusException;
 
 import ca.bc.gov.hlth.hnweb.converter.rapid.RPBSPCI0Converter;
 import ca.bc.gov.hlth.hnweb.converter.rapid.RPBSPMC0Converter;
-import ca.bc.gov.hlth.hnweb.exception.HNWebException;
 import ca.bc.gov.hlth.hnweb.model.rapid.RPBSPCI0;
 import ca.bc.gov.hlth.hnweb.model.rapid.RPBSPMC0;
 import ca.bc.gov.hlth.hnweb.model.rest.mspcontracts.ContractInquiryRequest;
