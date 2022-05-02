d251c471496daa289be3d3f9e81278c2ec4f9bad
Trevor Schiavone <trevor.schiavone@cgi.com>
2022-05-02 14:30:25 -0700

    Added sections to help page

--- frontend/src/views/Help.vue
+++ frontend/src/views/Help.vue
@@ -3,29 +3,35 @@
     <AppCol class="col6">
       <h1>General Help with MSP Direct</h1>
 
-      <h2>Using the Navigation Menu</h2>
+      <section aria-labelledby="helpSection1">
+        <h2 id="helpSection1">Using the Navigation Menu</h2>
+        The navigation bar towards the top of the screen displays categories of available business services.
+        <ul>
+          <li>Hovering over a category will display a dropdown of the services available in that category.</li>
+          <li>Selecting a service from the dropdown will take you to a page where you can then run a transaction associated with that service.</li>
+        </ul>
+      </section>
 
-      The navigation bar towards the top of the screen displays categories of available business services.
-      <ul>
-        <li>Hovering over a category will display a dropdown of the services available in that category.</li>
-        <li>Selecting a service from the dropdown will take you to a page where you can then run a transaction associated with that service.</li>
-      </ul>
+      <section aria-labelledby="helpSection2">
+        <h2 id="helpSection2">Bulletins</h2>
+        Administrators may post bulletins on the Home page to communicate important information regarding the MSP Direct application.
+        <ul>
+          <li>This page may be updated at any time with a new bulletin. Users are encouraged
+            to regularly review the Home page for new bulletins.</li>
+          <li>MSP Direct has several different kinds of users; a bulletin may be relevant to all users or specific user communities.</li>
+        </ul>
+      </section>
 
-      <h2>Bulletins</h2>
-      Administrators may post bulletins on the Home page to communicate important information regarding the MSP Direct application.
-      <ul>
-        <li>This page may be updated at any time with a new bulletin. Users are encouraged
-          to regularly review the Home page for new bulletins.</li>
-        <li>MSP Direct has several different kinds of users; a bulletin may be relevant to all users or specific user communities.</li>
-      </ul>
+      <section aria-labelledby="helpSection3">
+        <h2 id="helpSection3">Contact the MSP Direct Helpdesk</h2>
+        <p>
+          The <span class="fix-me">XXXXXX</span> Help Desk provides support for MSP direct Monday to Friday, 8:00AM to 4:30PM, excluding statutory holidays. <br />
+          <br />Email: <span class="fix-me">XXXXXX</span>
+          <br />Phone: <span class="fix-me">XXXXXX</span>
+          <br />Toll free: <span class="fix-me">XXXXXX</span>
+        </p>
+      </section>
 
-      <h2>Contact the MSP Direct Helpdesk</h2>
-      <p>
-        The <span class="fix-me">XXXXXX</span> Help Desk provides support for MSP direct Monday to Friday, 8:00AM to 4:30PM, excluding statutory holidays. <br />
-        <br />Email: <span class="fix-me">XXXXXX</span>
-        <br />Phone: <span class="fix-me">XXXXXX</span>
-        <br />Toll free: <span class="fix-me">XXXXXX</span>
-      </p>
     </AppCol>
   </AppRow>
 </template>
