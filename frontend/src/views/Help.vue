94a24747708c06b32fb734db941d7443f75b36b0
weskubo-cgi <wes.kubo@cgi.com>
2022-06-09 14:37:29 -0700

    Updated help contact info.

--- frontend/src/views/Help.vue
+++ frontend/src/views/Help.vue
@@ -1,6 +1,6 @@
 <template>
   <AppRow>
-    <AppCol class="col6">
+    <AppCol>
       <h1>General Help with MSP Direct</h1>
 
       <section aria-labelledby="helpSection1">
@@ -16,35 +16,34 @@
         <h2 id="helpSection2">Bulletins</h2>
         Administrators may post bulletins on the Home page to communicate important information regarding the MSP Direct application.
         <ul>
-          <li>This page may be updated at any time with a new bulletin. Users are encouraged
-            to regularly review the Home page for new bulletins.</li>
+          <li>This page may be updated at any time with a new bulletin. Users are encouraged to regularly review the Home page for new bulletins.</li>
           <li>MSP Direct has several different kinds of users; a bulletin may be relevant to all users or specific user communities.</li>
         </ul>
       </section>
 
       <section aria-labelledby="helpSection3">
-        <h2 id="helpSection3">Contact the MSP Direct Helpdesk</h2>
+        <h2 id="helpSection3">Contact the Ministry of Health Help Desk</h2>
+        <p>The Ministry of Health provides support for MSP direct Monday to Friday, 8:00AM to 4:30PM, excluding statutory holidays.</p>
         <p>
-          The <span class="fix-me">XXXXXX</span> Help Desk provides support for MSP direct Monday to Friday, 8:00AM to 4:30PM, excluding statutory holidays. <br />
-          <br />Email: <span class="fix-me">XXXXXX</span>
-          <br />Phone: <span class="fix-me">XXXXXX</span>
-          <br />Toll free: <span class="fix-me">XXXXXX</span>
+          For further information or support about MSP Direct business services, please contact:
+          <AppLabel>Email</AppLabel><a href="mailto:HLTH.HnetConnection@gov.bc.ca?subject=MSP Direct Help">HLTH.HnetConnection@gov.bc.ca</a>
         </p>
+        <p>For information about access and/or authentication, please contact:<AppLabel>Email</AppLabel> <a href="mailto:HLTH.Helpdesk@gov.bc.ca?subject=MSP Direct Help">HLTH.Helpdesk@gov.bc.ca</a></p>
+        <p><AppLabel>Local</AppLabel>(250) 952-1234</p>
+        <p><AppLabel>Toll Free Canada Wide</AppLabel>1 (888) 764-2323</p>
       </section>
-
     </AppCol>
   </AppRow>
 </template>
 
 <script>
-    export default {
-        name: 'help'
-    }
+import AppLabel from '../components/ui/AppLabel.vue'
+export default {
+  name: 'help',
+  components: { AppLabel },
+}
 </script>
 <style scoped>
-.fix-me {
-  background-color: yellow;
-}
 ul {
   margin-bottom: 20px;
 }
