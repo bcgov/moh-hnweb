b5a113b0a9fd7524b10f22c8f70084d94fd49c1a
daveb-hni <80367247+daveb-hni@users.noreply.github.com>
2022-08-22 14:15:49 -0700

    Updates to change cancel page

--- frontend/tests/e2e/pages/maintenance/ChangeCancelDatePage.js
+++ frontend/tests/e2e/pages/maintenance/ChangeCancelDatePage.js
@@ -1,15 +1,15 @@
 import { Selector } from 'testcafe'
 
-class ChangeEffectiveDate {
+class ChangeCancelDate {
   constructor() {
     this.groupNumberInput = Selector('#groupNumber')
     this.phnInput = Selector('#phn')
-    this.existingEffectiveDateInput = Selector('#dp-input-existingEffectiveDate')
-    this.newEffectiveDateInput = Selector('#dp-input-newEffectiveDate')
+    this.existingCancelDateInput = Selector('#dp-input-existingCancelDate')
+    this.newCancelDateInput = Selector('#dp-input-newCancelDate')
     this.submitButton = Selector('button[type="submit"]')
     this.clearButton = Selector('button[type="button"]')
     this.errorText = Selector('div').withAttribute('class', 'error-text')
   }
 }
 
-export default new ChangeEffectiveDate()
+export default new ChangeCancelDate()
