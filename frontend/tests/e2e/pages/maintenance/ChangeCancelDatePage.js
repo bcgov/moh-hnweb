a1ea3b2d2c9476bc42c0a208be959e43a99781a2
daveb-hni <80367247+daveb-hni@users.noreply.github.com>
2022-08-24 15:14:33 -0700

    update tests

--- frontend/tests/e2e/pages/maintenance/ChangeCancelDatePage.js
+++ frontend/tests/e2e/pages/maintenance/ChangeCancelDatePage.js
@@ -4,10 +4,10 @@ class ChangeCancelDate {
   constructor() {
     this.groupNumberInput = Selector('#groupNumber')
     this.phnInput = Selector('#phn')
-    this.existingCancelDateInput = Selector('#dp-input-existingCancelDate')
-    this.newCancelDateInput = Selector('#dp-input-newCancelDate')
+    this.existingCancelDateInput = Selector('#dp-input-existingCancellationDate')
+    this.newCancelDateInput = Selector('#dp-input-newCancellationDate')
     this.submitButton = Selector('button[type="submit"]')
-    this.clearButton = Selector('button[type="button"]')
+    this.clearButton = Selector('button[type="button"]').withText('Clear')
     this.errorText = Selector('div').withAttribute('class', 'error-text')
   }
 }
