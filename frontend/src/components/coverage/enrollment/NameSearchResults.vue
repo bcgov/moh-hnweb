82a6f62161105eda84baf58f01017fab952414ac
daveb-hni <80367247+daveb-hni@users.noreply.github.com>
2022-10-12 14:43:00 -0700

    8001 r50 allow adding user when not in results (#128)
    * Added navigation for results
    
    * allow deploy from a specific branch
    
    * Revert "allow deploy from a specific branch"
    
    This reverts commit 956334307762a19bbaba3fdddfdc25cef797b5cb.
    
    * Clarified comment
    
    * Updated style and improved layout
    
    * Clear alerts before navigating to new screen

--- frontend/src/components/coverage/enrollment/NameSearchResults.vue
+++ frontend/src/components/coverage/enrollment/NameSearchResults.vue
@@ -1,21 +1,54 @@
 <template>
   <div>
-    <p>Number of Matches: {{ candidates.length }}</p>
-    <div v-for="candidate in candidates">
-      <NameSearchCandidate :candidate="candidate" />
+    <p>
+      Number of Matches: <b>{{ numberOfMatches }}</b>
+    </p>
+    <div v-if="hasCandidates">
+      <NameSearchCandidate v-for="candidate in candidates" :candidate="candidate" :key="candidate.phn" />
     </div>
+    <hr />
+    <p>If the individual for whom you are searching is not listed, you can select "Create New PHN" or "Refine Search"</p>
+    <AppRow>
+      <AppButton @click="registerWithoutPHN" mode="secondary" type="button">Create New PHN</AppButton>
+      <AppButton @click="refineSearch" mode="secondary" type="button">Refine Search</AppButton>
+    </AppRow>
   </div>
 </template>
 <script>
 import NameSearchCandidate from './NameSearchCandidate.vue'
+import { useAlertStore } from '../../../stores/alert'
 
 export default {
   name: 'NameSearchResults',
   props: {
     candidates: Array,
   },
+  emits: ['set-page-action'],
   components: {
     NameSearchCandidate,
+  },
+  setup() {
+    return {
+      alertStore: useAlertStore(),
+    }
+  },
+  computed: {
+    numberOfMatches() {
+      return this.candidates ? this.candidates.length : 0
+    },
+    hasCandidates() {
+      return this.candidates && this.candidates.length > 0
+    },
+  },
+  methods: {
+    registerWithoutPHN() {
+      this.alertStore.dismissAlert()
+      this.$emit('set-page-action', 'REGISTRATION')
+    },
+    refineSearch() {
+      this.alertStore.dismissAlert()
+      this.$emit('set-page-action', 'NAME_SEARCH')
+    },
   },
 }
 </script>
