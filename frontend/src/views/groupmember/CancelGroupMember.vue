9328f3cf0d90f88322367f250b3a0030ce78bf2e
AnumehaSrivastava05 <anumeha.saran@gmail.com>
2022-11-22 14:45:13 -0700

    8297: Addressed review comments

--- frontend/src/views/groupmember/CancelGroupMember.vue
+++ frontend/src/views/groupmember/CancelGroupMember.vue
@@ -10,12 +10,10 @@
     <p>This is a dropdown list box with a default Cancel Reason of "Employer's Request." The other available Cancel Reason is "Out of Province Move."</p>
     <p>To change a cancel date, use the Change Cancel Date screen.</p>
     <p>
-      MSP requires the reason for cancellation, the person’s current address and, if the person has moved outside of BC, the date of the move. To complete a cancellation, you must submit a Coverage Cancellation form (HLTH 217, found at <a>www.gov.bc.ca/mspgroupplanadministratorforms</a>), or submit
-      a written request including your group number, employee account number(s) and name(s), and all the information required on the form to Health Insurance BC at:
+      MSP requires the reason for cancellation, the person’s current address and, if the person has moved outside of BC, the date of the move. To complete a cancellation, you must submit a Coverage Cancellation form (HLTH 217, found at
+      <a href target="_blank">www.gov.bc.ca/mspgroupplanadministratorforms</a>), or submit a written request including your group number, employee account number(s) and name(s), and all the information required on the form to Health Insurance BC at: <br />PO Box 9140 <br />Stn Prov Govt<br />Victoria
+      BC V8W 9E5
     </p>
-    <p>PO Box 9140</p>
-    <p>Stn Prov Govt</p>
-    <p>Victoria BC V8W 9E5</p>
     <p>You can use the PHN with the Get Contract Periods screen to verify the cancellation.</p>
   </AppHelp>
   <div id="cancelGroupMember" v-if="inputFormActive">
