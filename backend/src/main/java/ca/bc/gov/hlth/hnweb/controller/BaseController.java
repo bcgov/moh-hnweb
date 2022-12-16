9dc1266ad624546c478c16322f725d8867c18159
prit-cgi <prit.thakkar@cgi.com>
2022-12-16 17:42:33 -0400

    fixed indentation

--- backend/src/main/java/ca/bc/gov/hlth/hnweb/controller/BaseController.java
+++ backend/src/main/java/ca/bc/gov/hlth/hnweb/controller/BaseController.java
@@ -46,7 +46,7 @@ public abstract class BaseController {
 	 * @param identifier The value of the identifier
 	 */
 	protected void addAffectedParty(Transaction transaction, IdentifierType type, String identifier, AffectedPartyDirection direction) {
-		if(identifier == null || identifier.isBlank() || type == null || direction == null) {
+		if (identifier == null || identifier.isBlank() || type == null || direction == null) {
 			return;
 		}
 		auditService.createAffectedParty(transaction, type, identifier, direction);
