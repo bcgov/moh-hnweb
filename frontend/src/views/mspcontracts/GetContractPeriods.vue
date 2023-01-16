6c7c6bb0e53113476c8c2599631dd6588e91e2d3
prit-cgi <prit.thakkar@cgi.com>
2023-01-16 16:25:26 -0500

    8531 alter instructions content to reflect changes

--- frontend/src/views/mspcontracts/GetContractPeriods.vue
+++ frontend/src/views/mspcontracts/GetContractPeriods.vue
@@ -4,15 +4,11 @@
       Use this screen to retrieve the persons and coverage periods associated with a group member’s MSP contract. This can be used to verify that a particular business service has been correctly submitted, for example whether an individual was canceled from the group account. The PHN Lookup screen
       can be used to find a group member or dependent's PHN.
     </p>
-    <p>
-      If the transaction was successful, persons will be returned: 
-      <ul>
-        <li>the spouse(s), active and cancelled ·</li>
-        <li>the dependent(s)</li>
-        <li>the person whose PHN was entered on the input screen</li>
-        <li>the group member.</li>      
-      </ul>  
-    </p>
+    <p>If the transaction was successful, persons will be returned:</p>
+    <p>the spouse(s), active and cancelled ·</p>
+    <p>the dependent(s)</p>
+    <p>the person whose PHN was entered on the input screen</p>
+    <p>the group member.</p>
     <p>Coverage periods are returned in descending order of effective date, for each person.</p>
     <p>A maximum of 10 persons and 5 coverage periods per person can be returned.</p>
     <p>The Cancel Reason, if applicable, is either "ELIGIBLE" (means coverage is cancelled under that Group but the person is still an MSP beneficiary), "INELIGIBLE" (means the person is no longer an MSP beneficiary), or "DECEASED</p>
