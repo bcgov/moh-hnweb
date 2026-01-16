28e25e8b47e7d64ac2ffea5235e8fa4a16be9971
swoodiwi <36248028+swoodiwi@users.noreply.github.com>
2026-01-16 14:30:05 -0800

    Removed test for Transfer button

--- frontend/tests/e2e/tests/UnauthorizedTest.js
+++ frontend/tests/e2e/tests/UnauthorizedTest.js
@@ -14,9 +14,6 @@ test('Check Page content ', async (t) => {
   await t.expect(UnauthorizedPage.title.innerText).eql('Service Permissions not assigned')
 })
 
-test('Check Transfer button ', async (tc) => {
-  await tc.click(UnauthorizedPage.transferButton).expect(UnauthorizedPage.userTransferTitle.innerText).eql('Welcome to the MSP Direct Account Transfer')
-})
 
 test('Check More Info button ', async (tc) => {
   await tc.click(UnauthorizedPage.moreInfoButton).expect(UnauthorizedPage.moreInfoTitle.innerText).eql('MSP Direct').expect(UnauthorizedPage.moreInfoText.innerText).contains('MSP Direct is an online business service,')
