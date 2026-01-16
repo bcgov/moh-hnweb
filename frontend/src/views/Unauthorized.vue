444ce14cb9b17962c6404a8ecbdd3ec9fc579c2b
swoodiwi <36248028+swoodiwi@users.noreply.github.com>
2026-01-16 14:46:12 -0800

    Remove account transfer button - DEMO for PBCS (#209)
    * Removed Transfer button and instructions text
    
    * Removed test for Transfer button

--- frontend/src/views/Unauthorized.vue
+++ frontend/src/views/Unauthorized.vue
@@ -1,10 +1,6 @@
 <template>
   <h1>Service Permissions Required</h1>
   <p>Your log-in credentials are valid but you don't have Permissions to view or run transactions in the new MSP Direct. Please follow the instructions below to get permissions.</p>
-  <p>If you have previously accessed HN Web using a HealthNetBC ID you can transfer the permissions associated with that HN Web account to your new MSP Direct account using the Transfer button below.</p>
-  <AppButton class="btn-lg" id="transfer">
-    <a :href="userTransferURL">Transfer</a>
-  </AppButton>
   <br />
   <p>If you are a new user and want to request access to MSP Direct, please contact your organization's Access Administrator.</p>
   <AppButton class="btn-lg" id="moreInfo">
@@ -14,10 +10,6 @@
 <script>
 export default {
   name: 'Unauthorized',
-  data() {
-    return {
-      userTransferURL: config.USER_TRANSFER_URL || import.meta.env.VITE_USER_TRANSFER_URL,
-    }
-  },
+  
 }
 </script>
